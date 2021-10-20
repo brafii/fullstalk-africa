430bd5340f8b060942c8d41e4e279f4aa16d62af
Fiifi Ofori <fiifi56@gmail.com>
2021-10-20 12:59:28 +0000

    checking for password strength working

--- vfarmer/js/myscript.js
+++ vfarmer/js/myscript.js
@@ -3,9 +3,27 @@ let message = document.getElementById('message');
 let strength = document.getElementById('strength');
 
 
-element.addEventListener("click", function(){ 
+password.addEventListener("input", function(){ 
     
-    
+    if(password.value.length > 0){
+        message.style.display = 'block';
+    }
+    else{
+        message.style.display = 'none';
+    }
+
+    if(password.value.length < 4){
+        strength.innerHTML = 'weak';
+        message.style.color = '#FF0000';
+    }
+    else if(password.value.length >= 4 && password.value.length < 8){
+        strength.innerHTML = 'medium';
+        message.style.color = '#FFA500';
+    }
+    else if(password.value.length >= 8){
+        strength.innerHTML = 'strong';
+        message.style.color = '#00FF00';
+    }
 
 
 });
