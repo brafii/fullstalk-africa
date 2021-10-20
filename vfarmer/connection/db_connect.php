4d392239c5e95cf97912dff9cb7e688ad1192228
Fiifi Ofori <fiifi56@gmail.com>
2021-10-20 10:56:36 +0000

    database connection successful

--- vfarmer/connection/db_connect.php
+++ vfarmer/connection/db_connect.php
@@ -4,10 +4,10 @@ $username = "root";
 $password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=vfarmers", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=vfarmer", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+//   echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
